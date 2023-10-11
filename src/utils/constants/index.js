39530a80eb32d527a72b55e2a5ebadcab27766d4
Zeeshan <zeeshan@zenkoders.com>
2023-10-11 22:10:31 +0500

    minor fix

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -1,3 +1,3 @@
-export const API_URL =
-  'https://bbd1-110-93-226-98.ngrok-free.app/lilipad-8d334/us-central1/';
-// export const API_URL = 'https://us-central1-lilipad-8d334.cloudfunctions.net/';
+// export const API_URL =
+//   'https://bbd1-110-93-226-98.ngrok-free.app/lilipad-8d334/us-central1/';
+export const API_URL = 'https://us-central1-lilipad-8d334.cloudfunctions.net/';
